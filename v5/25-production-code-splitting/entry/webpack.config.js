bf5170c49d5378ad90aaaa21036de0b325829294
peterhpchen <peterhsinpingchen@gmail.com>
2021-07-11 00:12:39 +0800

    add ch4-6 production code splitting

--- v5/25-production-code-splitting/entry/webpack.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  mode: "none",
-  entry: {
-    main: "./src/index.js",
-    sub: "./src/sub.js",
-  },
-};
--- v5/25-production-code-splitting/split-chunks/webpack.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  mode: "none",
-  optimization: {
-    splitChunks: {
-      chunks: "all",
-    },
-  },
-};
